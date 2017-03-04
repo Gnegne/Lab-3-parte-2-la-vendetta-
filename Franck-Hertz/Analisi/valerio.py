6c6d9136671ca881b8a1244e0fbdeec35fc8f961
IanMaayrkas <vale.loma@hotmail.it>
2017-03-05 00:23:10 +0100

    Rotocazzilli

--- Franck-Hertz/Analisi/valerio.py
+++ Franck-Hertz/Analisi/valerio.py
@@ -5,6 +5,7 @@ import numpy as np, matplotlib.pyplot as plt, matplotlib as mpl
 from lab import *
 from iandons import *
 
+plt.close('all')
 # inizio cose
 
 oscilfile = os.path.join(folder, 'oscilloscopio', 'dati001.csv')
@@ -14,6 +15,7 @@ z = data_from_oscill(oscilfile, mode='xy')
 
 
 def shift(arr, pos):
+	hold.sort()
 	n = np.zeros(np.shape(arr))
 	for i in range(pos):
 		n[i] = arr[0]
@@ -22,6 +24,7 @@ def shift(arr, pos):
 
 
 def smoothing(hold, roll):
+	hold.sort()
 	newx = sum(hold.x.val[i:i-roll] for i in range(roll)) / roll
 	errx = np.sqrt(sum(hold.x.err[i:i-roll]**2 for i in range(roll))) / roll
 	newy = sum(hold.y.val[i:i-roll] for i in range(roll)) / roll
@@ -31,8 +34,9 @@ def smoothing(hold, roll):
 
 
 def deltas(hold, dist):
-	x = hold.x.val[dist-1:] - hold.x.val[:1-dist]
-	y = (hold.y.val[dist-1:] + hold.y.val[:1-dist]) / 2
+	hold.sort()
+	y = hold.y.val[dist-1:] - hold.y.val[:1-dist]
+	x = (hold.x.val[dist-1:] + hold.x.val[:1-dist]) / 2
 	errx = np.sqrt(hold.x.err[dist-1:]**2 + hold.x.err[:1-dist]**2) / 2
 	erry = np.sqrt(hold.y.err[dist-1:]**2 + hold.y.err[:1-dist]**2) / 2
 	return DataHolder(x, y, errx, erry)
