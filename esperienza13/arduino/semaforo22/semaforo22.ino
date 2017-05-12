4f3e7bff87986a06434864d2339a2c2ac0500dc6
Gnegne <robertoribatti@gmail.com>
2017-05-12 22:08:51 +0200

    dsfghj

--- esperienza13/arduino/semaforo22/semaforo22.ino
+++ esperienza13/arduino/semaforo22/semaforo22.ino
@@ -1,29 +1,37 @@
-// prova n.2 di macchina a stati finiti semaforo con buzzer che sona su giallo 
-//autore: Thomas Giannoni Roberto Ribatti
-//ultima modifica: 02/05/2017
+// Traffic light FSM on ARduino
 
-#define INP 12   //il nostro abilitatore
-#define LED1 9  //LED verde  
-#define LED2 10  //LED giallo
-#define LED3 11  //LED rosso
+#define INP      12    // pin enable
+#define LED1     9     // pin green led  
+#define LED2     10    // pin yellow led
+#define LED3     11    // pin red led
 
-int timev = 10;
-int timeg = 3;
-int timer = 10;
-int timegen = 1;
-int clockk = 500;
-int stato = 0;
-int tprec = 0;
-int tnow = 0;
-int tstato=0;
-int val = 0;
-//stato 0 = verde
-//stato 1 = giallo verde
-//stato 2 = rosso
-//stato 3 = giallo
-//stato 4 = spento
+#define timeg    10   // green light duration in clock period
+#define timey    3    // yellow light duration in clock period
+#define timer    10   // red light duration in clock period
+#define timeen   1    // yellow/off duration in clock period
+#define clockk   500  // clock period in ms
+
+/*
+  state 0 => green light
+  state 1 => green and yellow light at the same time
+  state 2 => red light
+  state 3 => yellow light
+  state 4 => switched-off
+*/
+
+int LED[3] = {LED1, LED2, LED3};
+int time_state[5] = {timeg, timey, timer, timeen, timeen};
+int state = 0;
+int out[5][3] = {{1,0,0},{1,1,0},{0,0,1},{0,1,0},{0,0,0}};
+
+int tnow = 0;   // current time
+int tprec = 0;  // time since the last click of the clock
+int tstate = 0; // time since the last state change
+bool enable = HIGH;
+
 
 void setup() {
+  // Initialization pin mode
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -31,81 +39,33 @@ void setup() {
 }
 
 void loop() {
-  // le variabili che ho usato
   tnow = millis();
-  if(tnow-tprec >clockk){
-    val = digitalRead(INP);
-    
-    if(val == HIGH & stato == 3 & tnow-tstato > timegen*clockk) {
-      stato = 4;
-      output(stato);
-      tstato = tnow;
+  if(ClockEnd(tnow, tprec)){ // verify if a clock period has passed
+    enable = digitalRead(INP);  // reading the enable state
+    if (EndState(state, tnow, tstate)){ // verify if the current state time has ended
+      state = NextState(state, enable); // switch to the next state
+      Output(state);  // generate output
+      tstate = tnow;  //setting the starting time of the current state
     }
-    if(val == HIGH & stato == 4 & tnow-tstato > timegen*clockk) {
-      stato = 3;
-      output(stato);
-      tstato=tnow;
-    }
-    if(val == HIGH & stato != 4 & stato !=3) {
-      stato = 3;
-      output(stato);
-      tstato=tnow;
-    }
-    if(val == LOW & stato == 0 & tnow-tstato > timev*clockk) {
-      stato = 1;
-      output(stato);
-      tstato=tnow;
-    }
-    if(val == LOW & stato == 1 & tnow-tstato > timeg*clockk) {
-      stato = 2;
-      output(stato);
-      tstato=tnow;
-    }
-    if(val == LOW & stato == 2 & tnow-tstato > timer*clockk) {
-      stato = 0;
-      output(stato);
-      tstato=tnow;
-    }
-    if(val == LOW & stato != 0 & stato !=1 & stato!=2) {
-      stato = 0;
-      output(stato);
-      tstato=tnow;
-    }
-  tprec=tnow;
+  tprec=tnow; //setting the click time
   }
 }
 
-void output(int stato){
-if (stato == 0){
-   digitalWrite(LED1, HIGH);
-   digitalWrite(LED2, LOW);
-   digitalWrite(LED3, LOW);
-  }
-if (stato == 1){
-   digitalWrite(LED1, HIGH);
-   digitalWrite(LED2, HIGH);
-   digitalWrite(LED3, LOW);
-  }    
-if (stato == 2){
-   digitalWrite(LED1, LOW);
-   digitalWrite(LED2, LOW);
-   digitalWrite(LED3, HIGH);
-  }
-if (stato == 3){
-   digitalWrite(LED1, LOW);
-   digitalWrite(LED2, HIGH);
-   digitalWrite(LED3, LOW);
-  } 
-  if (stato == 4){
-   digitalWrite(LED1, LOW);
-   digitalWrite(LED2, LOW);
-   digitalWrite(LED3, LOW);
-  }
-}   
-    
-    
-    
-    
-    
-    
+bool ClockEnd(int tnow, int tprec){ // TRUE if a clock period has passed
+  return tnow-tprec > clockk;
+}
 
+bool EndState(int state, int tnow, int tstate){ // TRUE if the current state has ended
+return tnow-tstate > time_state[state]*clockk;
+}
+
+int NextState(int state, bool enable){ //calculate next state from current state and enable level
+  if(enable) return (state+1)%3;  // when the enable is HIGH loop between 0, 1, 2
+  else  return 3+ state%2;  // when enable is LOW loop between 3 and 4
+}
+
+void Output(int stato){ // generate output from the state
+  for (int j=0; j<3; j++){
+    digitalWrite(LED[j], out[state][j]);
+  }
+}
